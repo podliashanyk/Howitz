917c7217c91dfa2798aafdbbacdba3d3d61ae1e7
Hanne Moa <hanne.moa@sikt.no>
2023-10-09 10:13:50 +0200

    Use hashed password

--- src/howitz/users/model.py
+++ src/howitz/users/model.py
@@ -1,6 +1,8 @@
 from flask_login import UserMixin
 from pydantic import BaseModel
 
+from .utils import encode_password, verify_password
+
 
 class User(UserMixin, BaseModel):
     username: str
@@ -12,11 +14,10 @@ class User(UserMixin, BaseModel):
 
     @staticmethod
     def encode_password(password):
-        # needs much magic!
-        return password
+        return encode_password(password)
 
     def check_password(self, password):
-        return self.password == self.encode_password(password)
+        return verify_password(password, self.password)
 
     def authenticate(self, password):
         if self.check_password(password):
--- src/howitz/users/utils.py
+++ src/howitz/users/utils.py
@@ -1,13 +1,25 @@
-from howitz import database
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import database
 
 
 __all__ = [
     "authenticate_user",
+    "encode_password",
+    "verify_password",
 ]
 
 
-def authenticate_user(username, password):
+def authenticate_user(username: str, password: str):
     user = database.get(username)
     if user and user.authenticate(password):
         return user
     return None
+
+
+def encode_password(password: str):
+    return generate_password_hash(password, "scrypt")
+
+
+def verify_password(password: str, password_hash: str):
+    return check_password_hash(password_hash, password)
