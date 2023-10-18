a0d89dd3998a71bb83e955224400d6d5671983e1
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2023-10-18 14:41:27 +0200

    Set debug to true in dev mode

--- src/howitz/__main__.py
+++ src/howitz/__main__.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     parser = cliargs.make_argument_parser()
     args = cliargs.parse_args(parser)
     config = Config.from_args(args)
-    app.run(host=str(config.listen), port=config.port)
+    app.run(host=str(config.listen), port=config.port, debug=config.devmode)
